e7f33686abce8cd1766495a8ac5851af6e21fee6
Renaud <renaud.perchey@gmail.com>
2020-01-24 14:14:11 +0100

    add service

--- src/main/java/cinema/controller/PersonController.java
+++ src/main/java/cinema/controller/PersonController.java
@@ -3,12 +3,20 @@ package cinema.controller;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import cinema.persistence.entity.Person;
+import cinema.service.IPersonService;
 
 public class PersonController {
-
+	
+	@Autowired
+	IPersonService personService;
+	
+	
+	
+	
 	@GetMapping
 	List<Person>getAllPersons() {
 		//TODO
