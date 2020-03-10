4fc5f17d3063db7df3d2e95f32db538e0925ee15
jules <jules.angkna@gmail.com>
2020-03-10 16:37:33 +0100

    modif config

--- src/main/java/cinema/config/WebSecurityConfig.java
+++ src/main/java/cinema/config/WebSecurityConfig.java
@@ -44,10 +44,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 			.antMatchers("/authenticate").permitAll()
 			.antMatchers("/api/movie").permitAll()
 			.antMatchers("/api/movie/byTitleContaining").permitAll()
+			.antMatchers("/api/person").permitAll()
+			.antMatchers("/api/person/byNamePartial").permitAll()
 			.antMatchers("/api/user").permitAll()
-
 			.antMatchers("/api/user/token").permitAll()
-
 			.anyRequest().authenticated().and()
 			.exceptionHandling()
 			.authenticationEntryPoint(jwtAuthenticationEntryPoint).and()
