3bbfe4643af09e2621fcffcae09bcf1baf0b31ee
jules <jules.angkna@gmail.com>
2020-03-16 11:21:13 +0100

    act

--- src/main/java/cinema/persistence/repository/ActRepository.java
+++ src/main/java/cinema/persistence/repository/ActRepository.java
@@ -1,14 +1,14 @@
 package cinema.persistence.repository;
 
 import java.util.Optional;
+import java.util.Set;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import cinema.persistence.entity.Act;
 import cinema.persistence.entity.ActId;
-import cinema.persistence.entity.Movie;
-import cinema.persistence.entity.Person;
 
 public interface ActRepository extends JpaRepository<Act, ActId>{
-	Optional<Act> findByMovieAndPerson (Movie movie, Person person);
+	Optional<Act> findByMovieIdMovieAndPersonIdPerson (int idMovie, int idPerson);
+	Set<Act> findByMovieIdMovie (int idMovie);
 }
